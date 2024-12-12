4c0b2e664c4db9090bed65f802da06ed98a76850
amnt <skillovoypresent@gmail.com>
2024-12-12 17:42:18 +0300

    init postzapros

--- app/src/main/java/com/example/ithand_education_platform_mobile/MainActivity.java
+++ app/src/main/java/com/example/ithand_education_platform_mobile/MainActivity.java
@@ -2,6 +2,10 @@ package com.example.ithand_education_platform_mobile;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -14,16 +18,45 @@ import retrofit2.Callback;
 import retrofit2.Response;
 
 public class MainActivity extends AppCompatActivity {
-
-
+    private EditText editTextLogin;
+    private EditText editTextPassword;
+    private Button buttonLogin;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        initViews();
         // Получаем экземпляр API
         ApiService apiService = RetrofitClient.getClient().create(ApiService.class);
+        buttonLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                PostRequest postRequest = new PostRequest(editTextLogin.getText().toString().trim(), editTextPassword.getText().toString().trim(), 3);
+                Call<PostResponse> call = apiService.createPost(postRequest);
+                call.enqueue(new Callback<PostResponse>() {
+                    @Override
+                    public void onResponse(Call<PostResponse> call, Response<PostResponse> response) {
+                        if (response.isSuccessful() && response.body() != null) {
+                            PostResponse postResponse = response.body();
+                            Log.d("POST_RESPONSE", "ID: " + postResponse.getId() +
+                                    ", Title: " + postResponse.getTitle());
+                            Toast.makeText(MainActivity.this, postResponse.getTitle() + postResponse.getBody(), Toast.LENGTH_LONG).show();
+                        } else {
+                            Log.e("POST_RESPONSE", "Ответ пустой или не успешный");
+                            Toast.makeText(MainActivity.this, "Ничего не произошло", Toast.LENGTH_LONG).show();
+                        }
+                    }
 
+                    @Override
+                    public void onFailure(Call<PostResponse> call, Throwable t) {
+                        Log.e("POST_RESPONSE", "Ошибка: " + t.getMessage());
+                        Toast.makeText(MainActivity.this, "не работает", Toast.LENGTH_LONG);
+                    }
+                });
+
+            }
+        });
         // Создаём объект запроса
         PostRequest postRequest = new PostRequest("Заголовок", "Текст поста", 1);
 
@@ -36,15 +69,23 @@ public class MainActivity extends AppCompatActivity {
                     PostResponse postResponse = response.body();
                     Log.d("POST_RESPONSE", "ID: " + postResponse.getId() +
                             ", Title: " + postResponse.getTitle());
+                    Toast.makeText(MainActivity.this, postResponse.getTitle() + postResponse.getBody(), Toast.LENGTH_LONG).show();
                 } else {
                     Log.e("POST_RESPONSE", "Ответ пустой или не успешный");
+                    Toast.makeText(MainActivity.this, "Ничего не произошло", Toast.LENGTH_LONG).show();
                 }
             }
 
             @Override
             public void onFailure(Call<PostResponse> call, Throwable t) {
                 Log.e("POST_RESPONSE", "Ошибка: " + t.getMessage());
+                Toast.makeText(MainActivity.this, "не работает", Toast.LENGTH_LONG);
             }
         });
+    }
+    private void initViews(){
+        editTextLogin = findViewById(R.id.editTextLogin);
+        editTextPassword = findViewById(R.id.editTextPassword);
+        buttonLogin = findViewById(R.id.buttonLogin);
     }
 }
--- app/src/main/java/com/example/ithand_education_platform_mobile/RetrofitClient.java
+++ app/src/main/java/com/example/ithand_education_platform_mobile/RetrofitClient.java
@@ -4,7 +4,7 @@ import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
 public class RetrofitClient {
-    private static final String BASE_URL = "http://192.168.201.82:8080/"; // Замените на ваш URL
+    private static final String BASE_URL = "https://jsonplaceholder.typicode.com/"; // Замените на ваш URL
     private static Retrofit retrofit = null;
 
     public static Retrofit getClient() {
